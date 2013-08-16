beb0c62ea18001f879f6a34f7d5e4fb209e6b430
tjlane <thomas.joseph.lane@gmail.com>
2013-08-16 14:04:33 -0700

    cleaned up scripts

--- test/xray/test_scripts.py
+++ test/xray/test_scripts.py
@@ -47,7 +47,7 @@ class TestShoot(object):
     def test_single_gpu(self):
         if not GPU: raise SkipTest
         if TRAVIS: raise SkipTest
-        cmd = 'shoot -s %s -n 1 -m 512 -o testshot.shot > /dev/null 2>&1' % self.file
+        cmd = 'odin.xray.shoot -s %s -n 1 -m 512 -o testshot.shot > /dev/null 2>&1' % self.file
         subprocess.check_call(cmd, shell=True)
         if not os.path.exists('testshot.shot'):
             raise RuntimeError('no output produced')
@@ -57,7 +57,7 @@ class TestShoot(object):
             
     def test_cpu(self):
         if TRAVIS: raise SkipTest
-        cmd = 'shoot -s %s -n 1 -m 1 -o testshot2.shot > /dev/null 2>&1' % self.file
+        cmd = 'odin.xray.shoot -s %s -n 1 -m 1 -o testshot2.shot > /dev/null 2>&1' % self.file
         subprocess.check_call(cmd, shell=True)
         if not os.path.exists('testshot2.shot'):
             raise RuntimeError('no output produced')
@@ -65,48 +65,13 @@ class TestShoot(object):
             s = xray.Shotset.load('testshot2.shot')
             os.remove('testshot2.shot')
         
-        
+@skip
 def test_plotiq():
     if not MPL: raise SkipTest
     if TRAVIS: raise SkipTest
-    cmd = 'plotiq -i %s -m 1.0 > /dev/null 2>&1' % ref_file('reference_shot.shot')
+    cmd = 'odin.xray.plotiq -i %s -m 1.0 > /dev/null 2>&1' % ref_file('reference_shot.shot')
     subprocess.check_call(cmd, shell=True)
     if not os.path.exists('intensity_plot.pdf'):
         raise RuntimeError('no output produced')
     else:
         os.remove('intensity_plot.pdf')
-        
-        
-def test_plotcorr():
-    if not MPL: raise SkipTest
-    if TRAVIS: raise SkipTest
-    cmd = 'plotcorr -i %s > /dev/null 2>&1' % ref_file('reference_shot.shot')
-    subprocess.check_call(cmd, shell=True)
-    if not os.path.exists('correlation_plot.pdf'):
-        raise RuntimeError('no output produced')
-    else:
-        os.remove('correlation_plot.pdf')
-        
-        
-def test_solvate():
-    if not OPENMM: raise SkipTest
-    if TRAVIS: raise SkipTest
-    cmd = 'solvate -i %s > /dev/null 2>&1' % ref_file('ala2.pdb')
-    subprocess.check_call(cmd, shell=True)
-    if not os.path.exists('solvated.pdb'):
-        raise RuntimeError('no output produced')
-    else:
-        o = trajectory.load('solvated.pdb')
-        os.remove('solvated.pdb')
-        
-        
-def test_cbf2shot():
-    if TRAVIS: raise SkipTest
-    cmd = 'cbf2shot -i %s -o test.shot > /dev/null 2>&1' % ref_file('test_cbf.cbf')
-    subprocess.check_call(cmd, shell=True)
-    if not os.path.exists('test.shot'):
-        raise RuntimeError('no output produced')
-    else:
-        o = xray.Shotset.load('test.shot')
-        os.remove('test.shot')
-        
