cb7d5766c14b397737137a988b7bd41c28f1fc0e
tjlane <thomas.joseph.lane@gmail.com>
2013-10-12 16:45:45 -0700

    updated exptdata to reflect conversation w CRS

--- src/python/potential.py
+++ src/python/potential.py
@@ -1,6 +1,6 @@
 
 """
-synth.py
+potential.py
 
 Experimental Data & Simulation Synthesis.
 
@@ -10,57 +10,94 @@ or mc.py) to run simulations. Further,
 
 """
 
-class ExptPotential(object):
+import abc
+
+from mdtraj import Trajectory
+from mdtraj import utils as mdutils
+
+class Potential(object):
     """
-A posterior potential that enforces a set of experimental constraints.
-"""
+    Attributes for a kind of experimental potential. Any potential that
+    inherets from this class can be sampled (integrated) by the samplers in
+    odin/sample.py.
+    """
     
-    def __init__(self, expt_data_collection):
-        
-        pass
-        
-        
+    __metaclass__ = abc.ABCMeta
+    
+    @abc.abstractmethod    
     def __call__(self, xyz):
         """
-Takes a set of xyz coordinates and evaluates the potential on that
-conformation.
-"""
-        
+        Takes a set of xyz coordinates and evaluates the potential on that
+        conformation.
+        """    
         return energy
     
         
-    @property
-    def converged(self):
-        return self._evaluate_convergence()
+class SingleParticlePotential(Potential):
+    
+    def __init__(self, expt_data, ):
+        
+        
+        return
     
         
-    def prior(self):
-        # need to think!
-        # this can be evaluated different ways depending on what kind of sampling
-        # gets performed. The best would be to have OpenMM plugged directly in
-        # here, but that might not be possible
-        pass
-    
+    def __call__(self, xyz):
+        """
+        Evaluate the potential of `xyz`.
+        
+        Parameters
+        ----------
+        xyz : mdtraj.Trajectory, np.ndarray
+            Either an MD trajectory or 3-d array describing (snapshots, atoms, xyz)
+            
+        Returns
+        -------
+        potential : np.ndarray, float
+            The potential for each frame of `xyz`
+        """
+        
+        if isinstance(xyz, trajectory.Trajectory):
+            coords = xyz.xyz
+        elif:
+            mdutils.ensure_type(xyz, np.float, 3, 'xyz', add_newaxis_on_deficient_ndim=True)
+            coords = xyz
+        else:
+            raise TypeError('`xyz` must be type {mdtraj.Trajectory, np.ndarray}')
+            
+        preds = self._expt_data_collection.predict(xyz)
+        potential = np.exp( 1/spreds
+
+        
+class EnsemblePotential(Potential):
+    """
+    A posterior potential that enforces a set of experimental constraints.
+    """
+
+        
+    @property
+    def lambdas_converged(self):
+        return self._evaluate_convergence()
+        
         
     def optimize_lambdas(algorithm='default'):
         """
-This is the only place where the experimentally measured data actually
-gets used.
-"""
-        pass
-    
-        
-    def fi(self, xyz):
+        This is the only place where the experimentally measured data actually
+        gets used.
         """
-Compute all the f_i values for a configuration `xyz`
-"""
         pass
     
-        
+
     def _hessian(self):
         
         pass
         
         
     def _evaluate_convergence(self):
+        pass
+        
+
+    def fi(self, xyz):
+        """
+        Compute all the f_i values for a configuration `xyz`
+        """
         pass
---
+++ src/python/xray/saxs.py
@@ -0,0 +1,19 @@
+
+
+"""
+saxs.py
+"""
+
+from exptdata import EnsembleExpt
+
+
+class SAXS(EnsembleExpt):
+    """
+    A collection of SAXS data
+    """
+    
+    def __init__(self):
+        
+
+        return
+
