722947c4f7e6d30976dd5d55a3781b3b963f36ed
tjlane <thomas.joseph.lane@gmail.com>
2014-03-17 09:04:46 -0700

    pruned all thor code

--- src/misc/debug/solidangle_run_test.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include <math.h>
-#include "solidangle.hh"
-
--- src/python/xray/__init__.py
+++
@@ -1,2 +0,0 @@
-# dump the xray.py namespace into the xray module namespace
-from xray import *
